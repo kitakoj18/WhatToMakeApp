6f8e09e14896928bc564c08d087912cb580f848a
kitakoj18 <kitakoj18@gmail.com>
2020-05-25 11:27:31 -0700

    started formatting instruction details

--- screens/SelectedRecipe.js
+++ screens/SelectedRecipe.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, Image } from 'react-native';
 import axios from 'axios';
 
 const SelectedRecipeModal = props => {
@@ -7,7 +7,7 @@ const SelectedRecipeModal = props => {
     const { navigation } = props;
 
     const [isLoading, setIsLoading] = useState(true);
-    const [recipeDetails, setRecipeDetails] = useState({});
+    const [recipeDetails, setRecipeDetails] = useState(null);
 
     useEffect(() => {
 
@@ -40,9 +40,36 @@ const SelectedRecipeModal = props => {
         )
     }
 
+    let recipeTitle;
+    let ingredientDetails;
+    let recipeLength;
+    let imgUrl;
+    let recipeInstructions;
+
+    if (!isLoading && recipeDetails) {
+        recipeTitle = recipeDetails.title;
+        console.log(recipeTitle)
+        // list of extended ingredients
+        ingredientDetails = recipeDetails.extendedIngredients;
+        recipeLength = recipeDetails.readyInMinutes;
+        imgUrl = recipeDetails.image;
+        recipeInstructions = recipeDetails.instructions;
+    }
+
     return (
-        <View>
-            <Text>{recipeDetails.instructions}</Text>
+        <View style={styles.detailsContainer}>
+            <View style={styles.titleContainer}>
+                <Text>{recipeTitle}</Text>
+            </View>
+            <View style={styles.imgContainer}>
+                <Image source={{ uri: imgUrl }} style={styles.img} />
+            </View>
+            <View style={styles.servingTimeContainer}>
+                <Text>Serving Time: {recipeLength} minutes</Text>
+            </View>
+            <View style={styles.instructionsContainer}>
+                <Text>{recipeInstructions}</Text>
+            </View>
         </View>
     )
 };
@@ -52,6 +79,36 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    detailsContainer: {
+        flex: 1,
+        height: 400,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10
+    },
+    titleContainer: {
+        height: '10%',
+        alignItems: 'center'
+    },
+    imgContainer: {
+        height: '50%',
+        borderRadius: 10,
+        overflow: 'hidden',
+        borderColor: 'black',
+        borderWidth: 1
+    },
+    img: {
+        height: '100%',
+        width: '100%'
+    },
+    servingTimeContainer: {
+        height: '10%',
+        alignItems: 'center'
+    },
+    instructionsContainer: {
+        height: '30%'
     }
 });
 
