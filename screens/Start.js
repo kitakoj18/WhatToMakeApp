f1799e11d6e0c45c73ed2b3e9d121d8a0f64cdb8
kitakoj18 <kitakoj18@gmail.com>
2020-06-03 12:09:18 -0700

    added background image to start page

--- screens/Start.js
+++ screens/Start.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native';
 
 import AppButton from '../components/AppButton';
 
 const StartScreen = props =>{
     return (
-        <View style={styles.screen}>
-            <View style={styles.textContainer}>
+        <ImageBackground
+            source={require('../assets/verticalwallpaper.jpg')}
+            style={styles.imageBackground}
+            imageStyle={{resizeMode: 'cover'}}
+        >
+            {/* <View style={styles.textContainer}>
                 <Text style={styles.text}>
                     I have ingredients in the fridge but I don't know what to cook...
                 </Text>
-            </View>
+            </View> */}
             <AppButton 
                 onPress={() =>{
                     props.navigation.navigate({
@@ -21,24 +25,31 @@ const StartScreen = props =>{
             >
                 Find something to cook!
             </AppButton>
-        </View>
+        </ImageBackground>
     )
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5F5DC'
+    imageBackground: {
+        flex:1, 
+        width: '100%', 
+        height: '100%', 
+        justifyContent: 'center', 
+        alignItems: 'center'
     },
     textContainer: {
         width: '70%',
         alignItems: 'center',
-        paddingBottom: 20
+        marginTop: 255,
+        marginBottom: 10,
+        // backgroundColor: '#800000'
+        // paddingBottom: 20,
+        // backgroundColor: '#FFDAB9'
     },
     text: {
         fontSize: 16,
+        color: '#FFDAB9',
+        fontWeight: 'bold',
         textAlign: 'center'
     },
     button: {
